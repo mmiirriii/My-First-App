577344e55665d4220e51d46a1c80f04a14087b2c
mmiirriii <paragatjunerose632@gmail.com>
2026-05-16 17:40:27 +0800

    added interactive profile card

--- MyFirstApp/app/(tabs)/index.tsx
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -1,19 +1,41 @@
 import { Image } from 'expo-image';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import MEEE from '../../assets/images/MEEE.jpg';
 
 export default function App() {
+  const [name, setName] = useState('');
   return (
     <View style={s.screen}>
-      <Image source={MEEE} style={s.photo} /> 
-      
-      <Text style={s.name}>June Rose Paragat</Text>
-      <Text style={s.bio}>3rd Year MMA Student at Mapúa Malayan Colleges Mindanao</Text>
+      <Image source={MEEE}
+      style={s.photo}
+    /> 
+
+    <TextInput 
+        placeholder="Type your name"
+        placeholderTextColor="#aaa"
+        onChangeText={setName}
+        value={name}
+        style={{
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+          width: 200,
+        }}
+      />
+
+    <Text style={s.name}>
+      Hello, {name}!
+    </Text>    
+
+    <Text style={s.bio}>
+      3rd Year MMA Student at Mapúa Malayan Colleges Mindanao
+      </Text>
     </View>
   );
 }
 // I change the stylesheet to make the profile screen look better. 05/16/2026
-
 const s = StyleSheet.create({
   screen: { 
     flex: 1, 
